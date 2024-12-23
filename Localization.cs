6c1ade39b46babb2d06c8f17dda2f363bf933916
thril <thril@DESKTOP-3MK3ML3>
2024-12-23 15:20:42 +0100

    Add project files.

---
+++ Localization.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Free_Food
+{
+    class Localization
+    {
+        public string enterHalt = "↓";
+        public string welcomeTo = "Welcome to ";
+        public string title = "Project Free Food";
+        public string introduction = "It looks like you're here for the first time... Whassa name?";
+        public string nameError = "Please enter a valid name. Your name may not contain more than 10 letters.";
+        public string myNameIs = "My name is";
+
+        public string playerData(string name, int cash, int health, int damage)
+        {
+            int currentHealth = health - damage;
+            return "Name: " + name + "\n    " + cash + "¥\n    Health: " + currentHealth + "/" + health;
+        }
+
+        public string nameSuccess(string playerName)
+        {
+            return playerName + " hmm? What an... Interesting name indeed. Now, I'm sure you'll find what you need! ";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Free_Food
+{
+    class Game
+    {
+        public static Localization sl = new Localization();
+        //public static Player player = new Player();
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        static void Start()
+        {
+            Console.WriteLine(sl.welcomeTo + sl.title + "\n" + sl.introduction);
+            A:
+            var rl = Console.ReadLine();
+            if (rl == null || rl == "" || rl.Length > 10)
+            {
+                Console.WriteLine(sl.nameError);
+                goto A;
+            }
+            Player.name = rl;
+            Console.WriteLine(sl.nameSuccess(Player.name) + sl.enterHalt);
+            Console.ReadKey();
+            for (int i = 0; i < sl.myNameIs.Length; i++)
+            {
+                Console.Write(sl.myNameIs[i]);
+                int delay = (int)(150 * Math.Pow(2, i / 4.0));
+                System.Threading.Thread.Sleep(delay);
+            }
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            System.Threading.Thread.Sleep(500);
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine(sl.playerData(Player.name, Player.cash, Player.health, Player.damage));
+        }
+    }
+    static class Player
+    {
+        public static string name = "";
+        public static int cash = 0;
+        public static int health = 20;
+        public static int damage = 0;
+    }
+}
