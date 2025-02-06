8939ae94e18bcb14093bbafb22cc38fedd841dce
Thrilla <thrilltech@outlook.de>
2025-02-06 11:55:34 +0100

    Added saving and loading, and prepared encounter types.

--- Localization.cs
+++ Localization.cs
@@ -15,6 +15,52 @@ namespace Project_Free_Food
         public string nameError = "Please enter a valid name. Your name may not contain more than 10 letters.";
         public string myNameIs = "My name is";
 
+        public string[] defaultEncounterStrings = new string[]
+        {
+            "You arrive at a " + locationType[new Random().Next(0, locationType.Length)] + ".",
+            "A " + badCreatureType[new Random().Next(0, badCreatureType.Length)] + " appears in front of you.",
+            "You find yourself in a "  + locationType[new Random().Next(0, locationType.Length)] + ".",
+            "A " + goodCreatureType[new Random().Next(0, goodCreatureType.Length)] + " approaches you.",
+            "You hear a strange noise coming from the distance."
+        };
+
+        public static string[] locationType = new string[]
+        {
+            "forest",
+            "city",
+            "desert",
+            "mountain",
+            "cave"
+        };
+
+        public static string[] badCreatureType = new string[]
+        {
+            "aggressive dog",
+            "feral cat",
+            "wild boar",
+            "angry bear",
+            "swarm of bees",
+            "venomous snake",
+            "rabid raccoon",
+            "homeless person",
+            "gang member",
+            "wild turkey"
+        };
+
+        public static string[] goodCreatureType = new string[]
+        {
+            "friendly shopkeeper",
+            "kind stranger",
+            "helpful tourist",
+            "neighborly postman",
+            "compassionate paramedic",
+            "trustworthy police officer",
+            "friendly library assistant",
+            "accommodating restaurant host",
+            "assisting social worker",
+            "kind-hearted park ranger"
+        };
+
         public string playerData(string name, int cash, int health, int damage)
         {
             int currentHealth = health - damage;
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Project_Free_Food
@@ -13,10 +14,40 @@ namespace Project_Free_Food
         //public static Player player = new Player();
         static void Main(string[] args)
         {
-            Start();
+            DetermineSave();
         }
 
-        static void Start()
+        public static void DetermineSave()
+        {
+            Player.LoadData();
+            if (Player.Data.Name == "")
+            {
+                FirstTimePlayer();
+            }
+            else
+            {
+                ContinueGame();
+            }
+        }
+
+        public static void ContinueGame()
+        {
+            Console.WriteLine(sl.welcomeTo + sl.title + " " + sl.enterHalt);
+            Console.ReadKey();
+            Console.WriteLine(sl.playerData(Player.Data.Name, Player.Data.Cash, Player.Data.Health, Player.Data.Damage));
+            while (true)
+            {
+                GenerateStoryelement();
+                Console.ReadKey();
+            }
+        }
+
+        static void GenerateStoryelement()
+        {
+            Console.WriteLine(sl.defaultEncounterStrings[new Random().Next(0, sl.defaultEncounterStrings.Length)]);
+        }
+
+        static void FirstTimePlayer()
         {
             Console.WriteLine(sl.welcomeTo + sl.title + "\n" + sl.introduction);
             A:
@@ -26,8 +57,8 @@ namespace Project_Free_Food
                 Console.WriteLine(sl.nameError);
                 goto A;
             }
-            Player.name = rl;
-            Console.WriteLine(sl.nameSuccess(Player.name) + sl.enterHalt);
+            Player.Data.Name = rl;
+            Console.WriteLine(sl.nameSuccess(Player.Data.Name) + sl.enterHalt);
             Console.ReadKey();
             for (int i = 0; i < sl.myNameIs.Length; i++)
             {
@@ -40,14 +71,44 @@ namespace Project_Free_Food
             System.Threading.Thread.Sleep(500);
             Console.BackgroundColor = ConsoleColor.Black;
             Console.Clear();
-            Console.WriteLine(sl.playerData(Player.name, Player.cash, Player.health, Player.damage));
+            Console.WriteLine(sl.playerData(Player.Data.Name, Player.Data.Cash, Player.Data.Health, Player.Data.Damage));
+            Player.SaveData();
+            ContinueGame();
         }
     }
-    static class Player
+    public static class Player
     {
-        public static string name = "";
-        public static int cash = 0;
-        public static int health = 20;
-        public static int damage = 0;
+        public static PlayerData Data { get; set; } = new PlayerData();
+
+        public static void SaveData()
+        {
+            var json = JsonSerializer.Serialize(Data);
+            File.WriteAllText("save.json", json);
+        }
+
+        public static void LoadData()
+        {
+            if (File.Exists("save.json"))
+            {
+                var json = File.ReadAllText("save.json");
+                Data = JsonSerializer.Deserialize<PlayerData>(json);
+            }
+            else
+            {
+                // Initialize default values if no save file exists
+                Data.Name = "";
+                Data.Cash = 0;
+                Data.Health = 0;
+                Data.Damage = 0;
+            }
+        }
+    }
+
+    public class PlayerData
+    {
+        public string Name { get; set; }
+        public int Cash { get; set; }
+        public int Health { get; set; }
+        public int Damage { get; set; }
     }
 }
